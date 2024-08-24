9638578d21ea686931cb52c5d86b44a055282d48
Florian Schulz <florian.schulz404@gmail.com>
2024-08-24 19:59:10 +0200

    ✅ updated initial test case (service alive)

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -2,46 +2,22 @@ import assert = require("node:assert");
 import { describe, it } from "node:test";
 import { getcii } from "..";
 
-interface Config {
-    url: string
+interface BasicResponse {
+    message: string,
+    data?: any
 }
 
-describe('json is valid', () => {
-    const config = getConfig();
+const baseUrl = 'http://localhost:3000/api';
 
-    it('has url', () => {
-        assert(config?.url !== undefined
-            && config.url   !== '', 'URL is defined');
-    })
-});
-
-describe('getcii data from api', () => {
-    const config = getConfig();
-
+describe('getcii dummy api started', () => {
     it('have valid data', async () => {
-        const result = await getcii(config?.url!);
+        const result = await getcii(baseUrl);
 
         assert(!result.err, 'response is valid (!err)');
         assert(result.response?.ok, 'response is valid (response.ok)');
-
-        const data: { message: string } = result.data;
+        
+        const data: BasicResponse = result.data;
         assert(typeof data.message, 'response ');
         assert(typeof data.message, 'result is valid');
     });
 });
-
-/**
- * @method gets the config file
- * @returns {Config | undefined} the config file
- * @author Flowtastisch
- * @memberof Aciiverse
- * @date 24.08.24
- */
-function getConfig(): Config | undefined {
-    try {
-        const config: Config = require('../../config.json');
-        return config
-    } catch (err) {
-        console.error(err)
-    }
-}
